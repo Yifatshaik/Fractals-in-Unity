9546662d5464df9d50e489d4546a0d8153c1fb50
FluffyKitten <31812480+Yifatshaik@users.noreply.github.com>
2018-09-21 21:31:41 -0400

    files to download

---
+++ scripts/Fractal.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fractal : MonoBehaviour {
+
+    public Mesh[] meshes;
+    public Material material;
+
+    public float spawnProbability;
+
+    public int maxDepth;
+
+    private int depth;
+
+    public float childScale;
+
+    private Material[,] materials;
+
+    public float maxRotationSpeed;
+    private float rotationSpeed;
+    public float maxTwist;
+
+    private void InitializeMaterials()
+    {
+        materials = new Material[maxDepth + 1, 2];
+        for (int i = 0; i <= maxDepth; i++)
+        {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i, 0] = new Material(material);
+            materials[i, 0].color = Color.Lerp(Color.white, Color.magenta, t);
+            materials[i, 1] = new Material(material);
+            materials[i, 1].color = Color.Lerp(Color.white, Color.red, t);
+        }
+        materials[maxDepth, 0].color = Color.yellow;
+        materials[maxDepth, 1].color = Color.cyan;
+    }
+
+    private void Start()
+    {
+        if (materials == null)
+        {
+            InitializeMaterials();
+        }
+        gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
+        gameObject.AddComponent<MeshRenderer>().material =
+            materials[depth, Random.Range(0, 2)];
+        if (depth < maxDepth)
+        {
+            StartCoroutine(CreateChildren());
+        }
+        rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+        transform.Rotate(Random.Range(-maxTwist, maxTwist), 0f, 0f);
+    }
+
+    private static Vector3[] childDirection =
+    {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+    };
+
+    private static Quaternion[] childOrientation =
+    {
+        Quaternion.identity,
+        Quaternion.Euler(0f,0f,-90f),
+        Quaternion.Euler(0f,0f,90f),
+        Quaternion.Euler(90f,0f,0f),
+        Quaternion.Euler(-90f,0f,0f)
+    };
+
+    private IEnumerator CreateChildren()
+    {
+        for(int i =0;i<childDirection.Length; i++)
+        {
+            if (Random.value < spawnProbability)
+            {
+                yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+                new GameObject("Fractal Child").AddComponent<Fractal>().Initialize(this, i);
+            }
+        }
+    }
+
+    private void Initialize(Fractal parent, int childIndex)
+    {
+        meshes = parent.meshes;
+        materials = parent.materials;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        spawnProbability = parent.spawnProbability;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        maxTwist = parent.maxTwist;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = childDirection[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientation[childIndex];
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
+    }
+}
---
+++ shader/Hologram.shader
@@ -0,0 +1,77 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+Shader "Unlit/Hologram"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_Color ("Color", Color) = (1, 0, 0, 1)
+		_Bias("Bias", Float) = 0
+		_ScanningFrequency ("Scanning Frequency", Float) = 100
+		_ScanningSpeed ("Scanning Speed", Float) = 100
+	}
+	SubShader
+	{
+		Tags { "Queue" = "Transparent" "RenderType" = "Transparent" }
+		LOD 100
+		Zwrite off
+		Blend SrcAlpha One
+		cull off
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				float4 objVertex: TEXCOORD1;
+			};
+
+			fixed4 _Color;
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _Bias;
+			float _ScanningFrequency;
+			float _ScanningSpeed;
+			
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.objVertex = mul(unity_ObjectToWorld,v.vertex);
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				col = _Color * max(0, cos(i.objVertex.y * _ScanningFrequency + _Time.x * _ScanningSpeed) + _Bias);
+				col *= 1 - max(0, cos(i.objVertex.x * _ScanningFrequency + _Time.x * _ScanningSpeed) + 0.9);
+				col *= 1 - max(0, cos(i.objVertex.z * _ScanningFrequency + _Time.x * _ScanningSpeed) + 0.9);
+				return col;
+			}
+			ENDCG
+		}
+	}
+}
